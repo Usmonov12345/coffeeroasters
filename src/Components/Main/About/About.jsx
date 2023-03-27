8ffa086a32c2c5d2c73521df2859e36c72188f31
Usmonov12345 <toxirusmonov04@gmail.com>
2023-03-27 22:26:47 +0500

    two end

--- src/Components/Main/About/About.jsx
+++ src/Components/Main/About/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Abonecom from './Abonecom/Abonecom'
+import Abtwocom from './Abtwocom/Abtwocom'
 
 function About() {
   return (
     <div>
       <Abonecom/>
+      <Abtwocom/>
     </div>
   )
 }
