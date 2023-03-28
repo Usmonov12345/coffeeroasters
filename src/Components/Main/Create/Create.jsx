e6cfd67182b637f3f0d4b00b7b490564947ff610
Usmonov12345 <toxirusmonov04@gmail.com>
2023-03-28 05:43:59 +0500

    and

--- src/Components/Main/Create/Create.jsx
+++ src/Components/Main/Create/Create.jsx
@@ -3,11 +3,13 @@ import Four from '../Home/Four/Four'
 import Plonecom from './Plonecom/Plonecom'
 import './Create.scss'
 import Pltwocom from './Pltwocom/Pltwocom'
+import Plthreecom from './Plthreecom/Plthreecom'
 function Create() {
   return (
     <div>
       <Plonecom/>
         <Pltwocom/>
+        <Plthreecom/>
     </div>
   )
 }
---
+++ src/Components/Main/Create/Plthreecom/Plthreecom.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import './Plthreecom.scss'
+function Plthreecom() {
+  return (
+    <div className='Plthreecom'>
+        <div className="container">
+            <div className="card">
+                <ul className="card__list">
+                    <li className="card__item1">
+                        
+                    </li>
+                     <li className="card__item2">
+                        
+                    </li>
+                     <li className="card__item3">
+                        
+                    </li>
+                </ul>
+                
+                <ul className="card__lisT">
+                    <li className="card__iteM">
+                        <h2 className='card__iteM__title'>
+                            01
+                        </h2>
+                        <p className="card__iteM__text1">
+                                Pick your
+                                coffee
+                        </p>
+                        <p className="card__iteM__text2">
+                            Select from our evolving range of artisan coffees. Our beans are ethically 
+                            sourced and we pay fair prices for them. There are new coffees in all profiles 
+                            every month for you to try out.
+                        </p>
+                    </li>
+                    
+                     <li className="card__iteM">
+                        <h2 className='card__iteM__title'>
+                            02
+                        </h2>
+                        <p className="card__iteM__text1">
+                               Choose the frequency
+                        </p>
+                        <p className="card__iteM__text2">
+                           Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.
+                        </p>
+                    </li>
+
+
+                     <li className="card__iteM">
+                        <h2 className='card__iteM__title'>
+                            03
+                        </h2>
+                        <p className="card__iteM__text1">
+                               Receive and enjoy!
+                        </p>
+                        <p className="card__iteM__text2">
+                           We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Plthreecom
